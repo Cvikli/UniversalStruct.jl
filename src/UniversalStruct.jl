71572c6be57c165a2a2064ceeeb094ecef72badd
Marcell <havlikmarcell@gmail.com>
2024-02-23 17:29:17 +0100

    Example is working

--- src/UniversalStruct.jl
+++ src/UniversalStruct.jl
@@ -1,28 +1,26 @@
 module UniversalStruct
 
 
-using InitLoadableStruct: InitableLoadable, init
+using InitLoadableStruct: InitableLoadable
 using ExtendableStruct: Extendable, extend!
 using PersistableStruct: Persistable, load_disk, save_disk
-using MemoizeTyped
 
 
 # TO overload!
-using InitLoadableStruct: load_data!
+using InitLoadableStruct: init, load_data!
 using ExtendableStruct: append, need_data_before, need_data_after, init_before_data, init_after_data
 using PersistableStruct: folder, glob_pattern, unique_filename, parse_filename, parse_args, score
 
-export cached_load, Universal
+export cached_load, load, Universal
 
 abstract type Universal <: InitableLoadable end
 
 # cached_load(obj)  where T <: InitableLoadable = @memoize_typed T load(obj) unique_args(obj)
-cached_load(t::Type{T}, args...; kw_args...)  where T <: InitableLoadable = @memoize_typed T load(t, args...; kw_args...)
+# cached_load(t::Type{T}, args...; kw_args...)  where T <: InitableLoadable = @memoize_typed T load(t, args...; kw_args...)
 load(t::Type{T}, args...; kw_args...)         where T <: InitableLoadable = load_data!(init(t, args...; kw_args...))
-load(t::Type{T}, args...; kw_args...)         where T <: Persistable      = load(init(t, args...; kw_args...))
-load(obj::T)                                  where T <: Persistable      = begin
+load(t::Type{T}, args...; kw_args...)         where T <: Universal      = load(init(t, args...; kw_args...))
+load(obj::T)                                  where T <: Universal      = begin
 	c = load_disk(obj)
-@show "WHATT??"
 	c, needsave = !isa(c, Nothing) ? extend!(obj,c) : (load_data!(obj), true)
 	@show needsave
 	needsave && save_disk(c, !isa(c, Nothing))
--- test/example.jl
+++ test/example.jl
@@ -5,6 +5,9 @@ using Revise
 using RelevanceStacktrace
 using UniversalStruct
 
+using MemoizeTyped
+
+
 mutable struct BasicExample <: Universal
 	config::String
 	fr::Int
@@ -16,7 +19,7 @@ BasicExample(conf,fr,to) = BasicExample(conf,fr,to, Float32[])  # This is for in
 UniversalStruct.load_data!(obj::BasicExample) = (obj.data = randn(Float32,obj.to-obj.fr); return obj)
 
 # The directory you want the object to persist.
-# The default is okay for the folder function ----> folder(obj::T)              where T <: Persistable = mkfolder_if_not_exist("./data")
+# The default is okay for the folder function ----> folder(obj::T)              where T = mkfolder_if_not_exist("./data")
 # The glob pattern that finds the files (You can use asterix to match custom fields)
 UniversalStruct.glob_pattern(obj::BasicExample)              = "BasicExample_$(obj.config)_*_*.jld2" # throw("Unimplemented... So basically to get the files list it is advised for you to build this.") #"$(T)_$(obj.config)_*_*"*".jld2"
 # The unqiue filename for your 
@@ -39,16 +42,16 @@ UniversalStruct.need_data_before(obj::BasicExample, c::BasicExample) = obj.fr < 
 UniversalStruct.need_data_after(obj::BasicExample,  c::BasicExample) = c.to < obj.to
 
 # Configure and init the (before/after) object that is able to download the right data with load_data
-UniversalStruct.init_before_data(obj::BasicExample, c::BasicExample) = init(T, obj.fr, c.fr, obj.config)
-UniversalStruct.init_after_data(obj::BasicExample,  c::BasicExample) = init(T, c.to, obj.to, obj.config)
+UniversalStruct.init_before_data(obj::BasicExample, c::BasicExample) = UniversalStruct.init(BasicExample, obj.config, obj.fr, c.fr)
+UniversalStruct.init_after_data(obj::BasicExample,  c::BasicExample) = UniversalStruct.init(BasicExample, obj.config, c.to, obj.to)
 
 
 
-cached_load(BasicExample, "test",30,43)
-cached_load(BasicExample, "test",30,48)
-cached_load(BasicExample, "test",21,48)
-cached_load(BasicExample, "test",2, 48)
-cached_load(BasicExample, "test",5, 45)
-cached_load(BasicExample, "test",9, 13)
-cached_load(BasicExample, "test",9, 12)
+load(BasicExample, "test",30,43)
+load(BasicExample, "test",30,48)
+load(BasicExample, "test",21,48)
+load(BasicExample, "test",2, 48)
+load(BasicExample, "test",5, 45)
+load(BasicExample, "test",9, 13)
+load(BasicExample, "test",9, 12)
 
